d754a8c44e268a5b7d725ce81772d67bec0d1155
MustafaR_Projects <150495517+MustafaRcodes@users.noreply.github.com>
2024-07-18 10:20:46 -0400

    Add files via upload

---
+++ The basics.sql
@@ -0,0 +1,10 @@
+/* THE BASICS 
+1)	SELECT (column name and can use aggregation here and ALIAS (AS) with aggregated column)
+2)	DISTINCT (retrieve unique value)
+3)	FROM (Table name)
+4)	WHERE (use with OR, AND & logical operators such as IN, LIKE, ILIKE, NOT LIKE, BETWEEN, =, <>, >, <, >=, <=)
+5)	GROUP BY (use GROUP BY with single or multiple aggregation functions like COUNT, SUM, AVG, MIN, MAX)
+6)	HAVING (filter aggerated output)
+7)	ORDER BY (sort the result either by ASC or DESC)  
+8)	LIMIT (set limit on number of rows returned)
+*/
